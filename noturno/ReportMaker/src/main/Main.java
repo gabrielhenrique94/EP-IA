9746eb7b409fd5829f43358ec3ec1e3e9fdee609
bubui <brunoimurozaki@gmail.com>
2015-04-22 14:30:50 -0300

    New library to read a configuration file [ini4j]
    Inserção da lib ini4j para ler arquivo de configuração dos parâmetros
    que serão utilizados.

--- noturno/ReportMaker/src/main/Main.java
+++ noturno/ReportMaker/src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Wini;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InvalidFileFormatException,
+			IOException {
+		Wini configIni = new Wini(new File("resources/config.ini"));
 
+		int hasInterface = configIni.get("startup", "interface", int.class);
+		if (hasInterface == 0) {
+			// TODO: continuo lendo e dou run no algoritmo...
+		} else {
+			// TODO: new InterfaceGráfica()
+		}
 	}
 
 }
