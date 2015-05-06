0b0f41be63d7196f5b07d0b839330a95c91b645f
annabreganholi <annabreganholi@hotmail.com>
2015-05-06 18:08:33 -0300

    LVQzin

--- matutino/redes_neurais/src/redes_neurais/LVQ.java
+++ matutino/redes_neurais/src/redes_neurais/LVQ.java
@@ -1,13 +1,20 @@
 package redes_neurais;
-
+import java.util.ArrayList;
 public class LVQ {
+	
+	private  ArrayList<double[]> entradas;
+	private int epocas;
+	private int numNeurNaCamadaEscondida;
 	//Cria vetor com valores para multiplicar os anteriores
 	public int[] multiplicadores = new int[10]; 
 	// Creio que o tamanho esteja errado, nao entendi muito bem como funciona ainda
 	
 	//metodo do lvq
-	public static void fazContas (int [] valores, int epocas){
-		
+	public LVQ(ArrayList<double[]>entrada, int epoca, int numNeuronios){
+		this.entradas = entrada;
+		this.epocas = epoca;
+		this.numNeurNaCamadaEscondida = numNeuronios;
 	}
+
 
 }
