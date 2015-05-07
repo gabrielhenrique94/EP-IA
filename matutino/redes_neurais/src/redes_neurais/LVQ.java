6e409e10139c1930d4746e3d35f9ca35143badc8
annabreganholi <annabreganholi@hotmail.com>
2015-05-06 21:48:45 -0300

    Algumas outras coisas "prontas"

--- matutino/redes_neurais/src/redes_neurais/LVQ.java
+++ matutino/redes_neurais/src/redes_neurais/LVQ.java
@@ -1,7 +1,9 @@
 package redes_neurais;
 import java.util.ArrayList;
+import java.util.Map;
 public class LVQ {
 	
+	private Map<Integer, ArrayList<double[]>> classes;
 	private  ArrayList<double[]> entradas;
 	private int epocas;
 	private int numNeurNaCamadaEscondida;
@@ -9,18 +11,46 @@ public class LVQ {
 	public int[] multiplicadores = new int[10]; 
 	// Creio que o tamanho esteja errado, nao entendi muito bem como funciona ainda
 	
-	//metodo do lvq
+	/**metodo do lvq**/
 	public LVQ(ArrayList<double[]>entrada, int epoca, int numNeuronios){
 		this.entradas = entrada;
 		this.epocas = epoca;
 		this.numNeurNaCamadaEscondida = numNeuronios;
 	}
 
+	public void inicializaVetoresPesos(){
+		for (int i=0; i<10; i++){
+			classes.put(i,toArrayList( entradas.get(i)));
+		}
+	}
+	
+	//Uma gambiarra até eu descobrir como coloco um item num arraylist num map diretamente 
+	//OBS sei que não funcionaria se eu fosse colocar mais de 1 elem D:
+	public ArrayList<double[]> toArrayList(double[] valor){
+		ArrayList<double[]> gambiarra = new ArrayList<double[]>();
+		gambiarra.add(valor);
+		return gambiarra;
+	}
+	
+	/**
+	 *Cria o Mapa com as classes 
+	 **/
+	
+	public void criaMapa(){
+		for (int i=0; i<10;i++)
+		classes.put(i, null);//BTW não sei ainda ao certo como vai adicionar os elem do arraylist aqui depois
+		
+	}
  //Usar 10 primeiros vetores como vetores de peso!
 	//Usar k-means
 	
 	
-	//Calcular a distancia pela medida Distancia Euclidiana
+	/**
+	 * Calcula a distancia pela medida Distancia Euclidiana
+	 * @param vetor
+	 * @param vetorPesos
+	 * @return
+	 */
 	public double caculaDistEuclidiana(double[] vetor, double[] vetorPesos){
 		double distancia;
 		double soma=0;
@@ -33,13 +63,32 @@ public class LVQ {
 		return distancia;
 		
 	}
-	//Calcular a distancia pela medida Distancia de Hamming
+	/**
+	 * Calcular a distancia pela medida Distancia de Hamming
+	 * @param vetor
+	 * @param vetorPesos
+	 * @return
+	 */
 	public double caculaDistHamming(double[] vetor, double[] vetorPesos){
 		return 0;
 	}
 	
-	//Calcular a distancia pela medida Distancia de Chebschev
+	/**
+	 * Calcular a distancia pela medida Distancia de Chebschev
+	 * @param vetor
+	 * @param vetorPesos
+	 * @return
+	 */
 	public double caculaDistChebs(double[] vetor, double[] vetorPesos){
+		return 0;
+	}
+	
+	/**
+	 * Calcula erro da Qualizacao
+	 * @return
+	 */
+	public double erroQuantiz(){
+		
 		return 0;
 	}
 }
