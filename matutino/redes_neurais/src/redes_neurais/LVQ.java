e18df9b4e5e20045b6cd2806831cb14da97d0a80
annabreganholi <annabreganholi@hotmail.com>
2015-05-06 21:18:52 -0300

    Dist Euclidiana

--- matutino/redes_neurais/src/redes_neurais/LVQ.java
+++ matutino/redes_neurais/src/redes_neurais/LVQ.java
@@ -16,5 +16,30 @@ public class LVQ {
 		this.numNeurNaCamadaEscondida = numNeuronios;
 	}
 
-
+ //Usar 10 primeiros vetores como vetores de peso!
+	//Usar k-means
+	
+	
+	//Calcular a distancia pela medida Distancia Euclidiana
+	public double caculaDistEuclidiana(double[] vetor, double[] vetorPesos){
+		double distancia;
+		double soma=0;
+		
+		//Calcular a parte do somatorio da funcao
+		for (int i=0; i<vetor.length-1; i++){ //Se não tirar a ultima posicao do vetor (classe) é length-1 se tirar eh so length
+			soma=soma+ Math.pow(vetor[i]-vetorPesos[i], 2);
+		}
+		distancia= Math.pow(soma, (1/2));
+		return distancia;
+		
+	}
+	//Calcular a distancia pela medida Distancia de Hamming
+	public double caculaDistHamming(double[] vetor, double[] vetorPesos){
+		return 0;
+	}
+	
+	//Calcular a distancia pela medida Distancia de Chebschev
+	public double caculaDistChebs(double[] vetor, double[] vetorPesos){
+		return 0;
+	}
 }
