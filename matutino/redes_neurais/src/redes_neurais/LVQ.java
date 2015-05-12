0762fa809c9f8ea64b1df0bf92d87772c4862a25
anactc <anactrino@outlook.com>
2015-05-11 22:17:45 -0300

    Mudancas Treinamento LVQ
    Mudei o treinamento, atualizando os pesos sinapticos
    O alfa esta sendo atualizado da maneira "correta" agora
    VERIFICAR se os pesos estao sendo atualizados corretamente
    VERIFICAR se o arg min esta certo (como to salvando e usando o pego
    dele)
    Verificar se xj e civ estao corretos
    
    Falta bastante coisa, tem de ver o CMeans e Erro de Quantizacao

--- matutino/redes_neurais/src/redes_neurais/LVQ.java
+++ matutino/redes_neurais/src/redes_neurais/LVQ.java
@@ -1,7 +1,11 @@
 package redes_neurais;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+
 public class LVQ {
 	
 	/**
@@ -11,24 +15,37 @@ public class LVQ {
 	private ArrayList<double[]> vetorPrototipos = new ArrayList<double[]>();
 	
 	private int teste=0;
+	
 	/**
 	 * Array de vetores de entrada
 	 */
 	private  ArrayList<double[]> entradas;
 	
+	/**
+	 * 
+	 */
 	private int epocas;
 	
 	/**
-	 * Numero de neuonios que a rede neural apresenta na camada escondida.
+	 * 
+	 */
+	private int max_epocas = 3823;
+	
+	/**
+	 * Numero de neuronios que a rede neural apresenta na camada escondida.
 	 */
 	private int numNeurNaCamadaEscondida;
 	
 	/**
 	 * Taxa de aprendizado. A taxa sempre inicia em 1 e vai diminuindo
+	 * Essa variavel e para apenas armazenar o valor inicial de alfa 
 	 */
 	private double alfaInicial = 1;
 	
-	
+	/**
+	 * Variavel para armazenar as mudancas da taxa de aprendizado
+	 * Inicia em 1 e vai diminuindo
+	 */
 	private double alfaRotativo = alfaInicial;
 	
 	/**
@@ -42,8 +59,7 @@ public class LVQ {
 	* n e o numero de conjutos de dados (entradas.size)
 	* indica a associacao de um dado a um grupo
 	* 0 e nao associado e 1 associado
-	 */
-	
+	**/
 	private int[][] matrizBool ;
 	//Cria vetor com valores para multiplicar os anteriores
 
@@ -65,7 +81,9 @@ public class LVQ {
 		teste=entradas.size();
 	}
 
-	
+	/**
+	 * Funcao de teste
+	 * **/
 	public void testa(){
 		System.out.println("---");
 		criaVetorPrototipos();
@@ -87,14 +105,12 @@ public class LVQ {
 			}
 			System.out.println();
 		}
-
 	}
 	
-
-	
-
 	/**
-	 * Inicializacao da matriz Uh com todos os elementos = 0, isso e, assumindo que nao estao associados a o grupo
+	 * Inicializacao da matriz Uh com todos os elementos = 0
+	 * Quando elemento = 0 - nao pertence
+	 * elemento = 1, pertence
 	 **/
 	public void inicializarMatrizBool(){
 		matrizBool = new int[10][entradas.size()];
@@ -106,71 +122,161 @@ public class LVQ {
 	}
 	
 	/**
-	 *Cria vetor de prototipos
+	 *Cria vetor de prototipos 
 	 **/
 	public void criaVetorPrototipos(){
 		for (int i=0; i<10; i++)
 		vetorPrototipos.add(i, entradas.get(i));
-		
 	}
 
 	//Usar k-means
 	
 	/**
-	 * metodo para fazer o cMeans
+	 * Metodo para fazer o cMeans
 	 */
 	public void cMeans(){
-		//Quant de partições : quantidadeClasses
+		//Determine a quantidade de particoes c
+		//Quant de particoes : quantidadeClasses
+		//Determine um valor pequeno e positivo para um erro máximo, epsilon , permitido no processo; ver se esse e bom
 		double erroMax= 0.015;//Ver se esse valor é bom
-		int contInterador = 0;//IDEM AO COMMENT DE CIMA
-		while(true){// NÃO É TRUE É C(T)-C(T-1)<=EPSOLON;
+		int contInterador = 0;//Ver se esse valor é bom
+		while(true){// NÃO É TRUE É C(T)-C(T-1)<=EPSILON;
 			contInterador++;
 			//atualiza Uh - matriz de particao
+				//1 se i - grupo = argm minimo 
+				//0 caso ao contrario
 			//atualiza C - conjunto de prototipos que esta no mapa - vetor prototipo
 			
 		}
 	}
 	
 	
+	public static double[] somaDeVetores(double[] vetor1, double[] vetor2){
+		double[] res = new double[vetor1.length];
+		for(int i = 0 ; i < res.length;i++)
+			res[i] = vetor1[i] + vetor2[i];
+		return res;
+	}
+	
+	public static double[] subtracaoDeVetores(double[] vetor1, double[] vetor2){
+		double[] res = new double[vetor1.length];
+		for(int i = 0 ; i < res.length;i++)
+			res[i] = vetor1[i] - vetor2[i];
+		return res;
+	}
+		
+	public static double[] multiplicaAlfa(double[] vetor, double alfa){
+			double res[] = new double[vector.length];
+			for(int i = 0; i < res.length; i++)
+				res[i] = vetor[i] * alfa;
+			return res;
+	}
+	
 	/**
-	 * Funcao para treinar a rede
+	 * Funcao para treinamento da rede
 	 */
 	public void treinamentoLVQ(){
 		
-	//	inicializarMatrizBool();
-	//	criaVetorPrototipos();
+		//	inicializarMatrizBool();
+		//	criaVetorPrototipos();
+		
+		/**
+		 * Inicializando o conjunto de protótipos
+		 */
+		//	inicializarMatrizBool();
+		//	criaVetorPrototipos();
+		
+		/**
+		 * Determine o coeficiente de aprendizado alfa	
+		 */
+		
+		
+		/**
+		 * Determine o rótulo r de cada vetor protótipo
+		 * Rotulo e o Integer do mapa
+		 */
 
-		//Rotulo e o Integer do mapa
-		while(countTest<=3823){//determinar condicao de parada - numero fixo de iteracoes ou valor min p/ taxa de aprendizado
-			for(int j=0;j<entradas.size() ;j++){
-				atualizaMatrizBool (j); //encontrar o prototipo vencedor
-				//Adaptar pesos sinápticos
+		/**
+		 * Determinacacao de condicao de parada
+		 * Numero Fixo de iteracoes (max_Epocas) ou valor minimo taxa de aprendizado(alfaRotativo)
+		 */
+
+		//nesse caso = numero fixo de iteracoes
+		while(epocas < max_epocas){
+			for(int j=0;j<entradas.size(); j++){
+				//encontrar o prototipo vencedor - certeza q e isso?
+				atualizaMatrizBool(j); 
+				int a = argMin(j); //tem de encontrar a distancia minima - Iv
+				
+				/**
+				 * Adaptar pesos sinapticos 
+				 * pesos sinapticos = coordenadas dos vetores prototipos
+				 */
+				
+				/**se ((classe correta para o vetor treinamento = classe representada pela j-ésima unidade de saída)
+			 	* entao 
+			 	* classe representada pela j-ésima unidade de saída(nova) = classe representada pela j-ésima unidade de saída(velha) + 
+			 	* ((alfa-coeficiente de aprendizado) * (vetor de treinamento - classe representada pela j-ésima unidade de saída(velha)))
+			 	* 
+			 	* se Xj = civ
+			 	* sendo Xj rotulo associado ao dado xj entradas.get(j)
+			 	* CIv rotulo associado ao prototipo vencedor - vetorPrototipos(a)
+			 	* entao
+			 	* 
+			 	*/
+				//Observacao - tem um t nos slides da Sara q eu nao entendi bem onde ele se encaixaria - na atualizacao de pesos
+				//Observacao 2 - tem de verificar se xj e civ esta certo
+				
+				double[] xj = entradas.get(j);
+				double[] civ = vetorPrototipos.get(a);
+				if(xj == civ){ //verificacao
+					//adaptando peso sinaptico
+					civ = somaDeVetores(civ, multiplicaAlfa((subtracaoDeVetores(xj, civ)), alfaRotativo));
+				}
+				/**
+				 * senao
+				 * classe representada pela j-ésima unidade de saída(nova) = classe representada pela j-ésima unidade de saída(velha) -
+				 * ((alfa-coeficiente de aprendizado) * (vetor de treinamento - classe representada pela j-ésima unidade de saída(velha)))
+				 */
+				else{
+					//adaptando peso sinaptico
+					civ = subtracaoDeVetores(civ, multiplicaAlfa((subtracaoDeVetores(xj, civ)), alfaRotativo));
+				}
 			}
-			atualizaAlfaSimples();
+			/**
+			 * Reduzir a taxa de aprendizado
+			 */
+			//atualizaAlfaSimples();
+			alfaRotativo = atualizaAlfaMonot(epocas, max_epocas);
+	}
+		
+		//nesse caso = valor minimo taxa de aprendizado
+		while(alfaRotativo >= 0){
+			
 		}
 	}
+	
 	/**
-	 * Funcao para atualizar matris bool
+	 * Funcao para atualizar matriz booleana
 	 * @param j
 	 */
 	public void atualizaMatrizBool(int j){
 		int classe = argMin(j);
 		matrizBool[classe][j]=1;
 		countTest++;
-	
 	}
 	
 	/**
 	 * Funcao que calcula o arg min 
+	 * retorna o vetor prototipo mais proximo do dado
 	 * @param j
 	 * @return
 	 */
-
 	public int argMin(int j){
 		double distMin = 100000;
 		double dist =0;
 		int classe=-1;
-		for ( int i=0; i<vetorPrototipos.size() ; i++ ){
+		for (int i=0; i<vetorPrototipos.size(); i++ ){
 			dist = caculaDistEuclidiana(entradas.get(j), vetorPrototipos.get(i));
 			System.out.println("TESTE");
 			if (dist <= distMin){
@@ -180,11 +286,11 @@ public class LVQ {
 		}
 		System.out.println();
 		System.out.println(countTest);
-		return classe;//Tem que implementar, ainda não sei que estrutura usar x_x
+		return classe;//Tem que implementar, ainda não sei que estrutura usar 
 	}
 	
 	/**
-	 *Metodo da fuzzy c-means (nao sei se vai usar x_X)
+	 *Metodo da fuzzy c-means (nao sei se vai usar)
 	 * @param m
 	 */
 	/* Talvez nao vá precisar usar
@@ -204,10 +310,10 @@ public class LVQ {
 	*/
 	
 	/**
-	 * Funcao para atualizar o alfazito :~
+	 * Funcao para atualizar o alfa
 	 */
 	public void atualizaAlfaSimples(){
-		alfaRotativo=alfaRotativo/2;
+		alfaRotativo=alfaRotativo/2; //verificar	
 	}
 	
 	/**
@@ -215,8 +321,9 @@ public class LVQ {
 	 * @param interacao
 	 * @param interacaoMax
 	 */
-	public void atualizaAlfaMonot(int interacao, int interacaoMax){
-		alfaRotativo= alfaInicial*(1-(interacao/interacaoMax)); // Confirmar fucao!!
+	public double atualizaAlfaMonot(int interacao, int interacaoMax){
+		alfaRotativo= alfaInicial*(1.0-((double)interacao))/((double)interacaoMax); // Confirmar funcao!!
+		return alfaRotativo;
 	}
 	
 	/**
@@ -238,7 +345,6 @@ public class LVQ {
 		System.out.println(distancia);
 		
 		return distancia;
-		
 	}
 	
 	/**
@@ -248,6 +354,7 @@ public class LVQ {
 	 * @return
 	 */
 	public double caculaDistHamming(double[] vetor, double[] vetorPesos){
+		//completar
 		return 0;
 	}
 	
@@ -258,14 +365,16 @@ public class LVQ {
 	 * @return
 	 */
 	public double caculaDistChebs(double[] vetor, double[] vetorPesos){
+		//completar
 		return 0;
 	}
 	
 	/**
-	 * Calcula erro da Qualizacao
+	 * Calcula erro da Quantizacao
 	 * @return
 	 */
 	public double erroQuantiz(){
+		//completar
 		
 		return 0;
 	}
